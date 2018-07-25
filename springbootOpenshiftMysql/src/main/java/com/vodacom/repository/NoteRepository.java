c9c7dc9bf530bb6e7432831a61e1ad70dd728a77
mmittapelly123 <34439602+mmittapelly123@users.noreply.github.com>
2018-07-25 15:04:28 +0200

    Add files via upload

---
+++ springbootOpenshiftMysql/src/main/java/com/vodacom/repository/NoteRepository.java
@@ -0,0 +1,12 @@
+package com.vodacom.repository;
+
+
+import com.vodacom.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
